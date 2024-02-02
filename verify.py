1dc7a31176418ee9877e9c23f403cb568a8fb8e9
Bartosz Nowak <bartekn337@gmail.com>
2024-02-02 18:13:41 +0100

    change submodules

--- verify.py
+++ verify.py
@@ -16,7 +16,7 @@ log_and_run([
 ], "Preparing input", cwd="cairo-lang")
 
 log_and_run([
-    "cairo-compile --cairo_path=./src src/starkware/cairo/cairo_verifier/layouts/all_cairo/cairo_verifier.cairo --output cairo_verifier.json --no_debug_info", 
+    "cairo-compile --cairo_path=./src src/starkware/cairo/cairo_verifier/layouts/all_cairo/cairo_verifier.cairo --output cairo_verifier.json", 
 ], "Compiling verifier program", cwd="cairo-lang")
 
 log_and_run([
